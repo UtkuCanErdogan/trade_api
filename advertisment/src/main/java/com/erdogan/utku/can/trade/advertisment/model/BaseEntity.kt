23648b47d837ed79cdb5037b28e244189ddff6dd
UtkuCanErdogan <87581099+UtkuCanErdogan@users.noreply.github.com>
2021-09-30 22:16:28 +0300

    Düzenleme
    Fazla olan Adversiment modülü silindi

--- advertisment/src/main/java/com/erdogan/utku/can/trade/advertisment/model/BaseEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.erdogan.utku.can.trade.advertisment.model
-
-import java.time.LocalDateTime
-
-data class BaseEntity(
-        val createdDate : LocalDateTime? = null,
-        val updatedDate : LocalDateTime? = null
-)
